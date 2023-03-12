a06af3b05b09bd160f6d96a4bb6cae276ba95b09
Matv <97804343+mmsuzumm@users.noreply.github.com>
2023-03-12 18:58:12 +0300

    Working with state

--- big-react-test/src/App.js
+++ big-react-test/src/App.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import PetInfo from './components/PetInfo';
+import RandomNumber from './components/RandomNumber';
+import Button from './components/Counter/Button';
+import Counter from './components/Counter/Counter';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
+      <Counter count={count} />
+      <Button onClick={incrementCount} />
+      <Button onClick={incrementCount} />
+      <Button onClick={incrementCount} />
+      <RandomNumber range={1000} />
       <PetInfo pet="Cat" age={5} hasPet />
       <PetInfo pet="Dog" age={3} hasPet />
       <PetInfo hasPet={false} />
---
+++ big-react-test/src/components/Counter/Button.js
@@ -0,0 +1,5 @@
+function Button({ onClick }) {
+  return <button onClick={onClick}>Button that does nothing</button>;
+}
+
+export default Button;
