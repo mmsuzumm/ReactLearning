427c2c483759237f35be92d97b8269af167c7d2d
Matv <97804343+mmsuzumm@users.noreply.github.com>
2023-03-12 13:34:14 +0300

    return jsx by condition

--- big-react-test/src/App.js
+++ big-react-test/src/App.js
@@ -4,9 +4,9 @@ import PetInfo from './components/PetInfo';
 function App() {
   return (
     <div className="App">
-      <PetInfo pet="Cat" age={5} />
-      <PetInfo pet="Dog" age={3} />
-
+      <PetInfo pet="Cat" age={5} hasPet />
+      <PetInfo pet="Dog" age={3} hasPet />
+      <PetInfo hasPet={false} />
     </div>
   );
 }
--- big-react-test/src/components/PetInfo.js
+++ big-react-test/src/components/PetInfo.js
@@ -1,9 +1,10 @@
 function PetInfo(props) {
-  const { pet, age } = props;
-  return (
-    <h1>
-      {pet} is {age} years old
-    </h1>
+  const { pet, age, hasPet } = props;
+
+  return hasPet ? (
+    <h1>{`${pet} is ${age} years old`}</h1>
+  ) : (
+    <h1>I don't have an animal</h1>
   );
 }
 
